48a2557add3e24bf9a5bd539ea3f2cd1ac0464bc
Tobias Umbach <umbach@louis.info>
2016-02-09 14:23:34 +0100

    Fix image 404'ing

--- lib/redmine_rate/hooks/view_layouts_base_html_head_hook.rb
+++ lib/redmine_rate/hooks/view_layouts_base_html_head_hook.rb
@@ -2,7 +2,12 @@ module RedmineRate
   module Hooks
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return content_tag(:style, "#admin-menu a.rate-caches { background-image: url('#{image_path('database_refresh.png', :plugin => 'redmine_rate')}'); }", :type => 'text/css')
+        css = <<-CSS
+          #admin-menu a.rate-caches {
+            background-image: url('/plugin_assets/redmine_rate/images/database_refresh.png');
+          }
+        CSS
+        content_tag(:style, css.html_safe, type: "text/css")
       end
     end
   end
