7349e123ebcac3bf1358b781430c0b0d634120a8
Tobias Umbach <umbach@louis.info>
2016-02-15 12:45:15 +0100

    Rewrite routing test to use Redmine::RoutingTest

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -1,16 +1,14 @@
-require "#{File.dirname(__FILE__)}/../test_helper"
+require_relative "../test_helper"
 
-class RoutingTest < ActionController::IntegrationTest
-  context "routing rates" do
-    should_route :get, "/rates", controller: "rates", action: "index"
-    should_route :get, "/rates/new", controller: "rates", action: "new"
-    should_route :get, "/rates/1", controller: "rates", action: "show", id: "1"
-    should_route :get, "/rates/1/edit", controller: "rates", action: "edit", id: "1"
+class RoutingTest < Redmine::RoutingTest
+  test "routing rates" do
+    should_route "GET /rates"        => "rates#index"
+    should_route "GET /rates/new"    => "rates#new"
+    should_route "GET /rates/1"      => "rates#show", :id => "1"
+    should_route "GET /rates/1/edit" => "rates#edit", :id => "1"
 
-    should_route :post, "/rates", controller: "rates", action: "create"
-
-    should_route :put, "/rates/1", controller: "rates", action: "update", id: "1"
-
-    should_route :delete, "/rates/1", controller: "rates", action: "destroy", id: "1"
+    should_route "POST /rates"       => "rates#create"
+    should_route "PUT /rates/1"      => "rates#update", :id => "1"
+    should_route "DELETE /rates/1"   => "rates#destroy", :id => "1"
   end
 end
