2e8fc494862ebdcbfa21c922fbce158d1bc196c8
Alexander Meindl <a.meindl@alphanodes.com>
2016-08-14 19:54:08 +0200

    change label for better style

--- lib/redmine_rate/hooks/plugin_timesheet_views_timesheet_group_header_hook.rb
+++ lib/redmine_rate/hooks/plugin_timesheet_views_timesheet_group_header_hook.rb
@@ -2,7 +2,7 @@ module RedmineRate
   module Hooks
     class PluginTimesheetViewsTimesheetGroupHeaderHook < Redmine::Hook::ViewListener
       def plugin_timesheet_views_timesheet_group_header(_context = {})
-        content_tag(:th, l(:rate_cost), width: '8%')
+        content_tag(:th, l(:label_rate_cost), width: '8%')
       end
     end
   end
