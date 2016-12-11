400860681bbdf3c0aea9be92e3d94e5fab064ecc
Alexander Meindl <a.meindl@alphanodes.com>
2016-12-11 08:10:26 +0100

    Version bump

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_rate do
   url 'https://github.com/alphanodes/redmine_rate'
   description 'The Rate plugin provides an API that can be used to find the rate for a Member of a Project at a specific date.
                It also stores historical rate data so calculations will remain correct in the future.'
-  version '0.2.2-dev'
+  version '0.2.2'
 
   requires_redmine version_or_higher: '3.3.0'
 
